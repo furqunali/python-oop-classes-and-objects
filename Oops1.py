b1deb2bddf32db04572c2a22f24f47d98adead5d
furqunali <furqanali2628@gmail.com>
2025-05-22 07:14:47 +0500

    Add files via upload

---
+++ Oops1.py
@@ -0,0 +1,14 @@
+#Object Oriented Programing
+#Class is a blueprint for creating objects
+class Student:
+    name = "Kamran"
+s1 = Student()
+print(s1)
+print(s1.name)
+
+
+class Car:
+    color = "blue"
+
+car1 = Car()
+print(car1.color)   
